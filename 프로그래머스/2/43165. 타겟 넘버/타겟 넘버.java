9115f4dceff804647cd9555b9fe242336594e54b
Yeong Hwan Jung <160090871+Jungyh0@users.noreply.github.com>
2025-05-11 08:38:58 +0900

    [level 2] Title: 타겟 넘버, Time: 187.59 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,26 +4,25 @@ class Solution {
    public int solution(int[] numbers, int target) {
         int answer = 0;
         int length = numbers.length;
-        Queue<Integer> q = new LinkedList<>();
-        
-        q.offer(numbers[0]);
-        q.offer(-numbers[0]);
-        
-        for (int i = 1; i < length; i++) {
-            int size = q.size();
-            for (int j = 0; j < size; j++) {
-                int currentSum = q.poll();
-                q.offer(currentSum + numbers[i]);
-                q.offer(currentSum - numbers[i]);
+       Queue<Integer> q = new LinkedList<>();
+       
+       q.offer(numbers[0]);
+       q.offer(-numbers[0]);
+       
+       for (int i = 1; i < length; i ++){
+           int size = q.size();
+           for (int j = 0; j < size; j ++){
+               int cur = q.poll();
+               q.offer(cur + numbers[i]);
+               q.offer(cur - numbers[i]);
             }
         }
-        
-        while (!q.isEmpty()) {
-            if (q.poll() == target) {
-                answer++;
-            }
-        }
-        
+       
+       while (!q.isEmpty()){
+           if (q.poll() == target){
+               answer ++;
+           }
+       }
         return answer;
     }
 }
