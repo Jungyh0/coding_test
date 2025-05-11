a91b57485ff070f2cd4ed74e423780e9559c46b9
Yeong Hwan Jung <160090871+Jungyh0@users.noreply.github.com>
2025-05-11 21:47:52 +0900

    [level 2] Title: 올바른 괄호, Time: 17.94 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,8 +4,8 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++){
+        
+        for (int i = 0; i < s.length(); i ++){
             if (s.charAt(i) == '('){
                 stack.push(0);
                 continue;
@@ -14,7 +14,7 @@ class Solution {
                 return false;
             stack.pop();
         }
-        if (!stack.isEmpty()) return false;
+        if (!stack.isEmpty())   return false;
         return answer;
     }
 }
