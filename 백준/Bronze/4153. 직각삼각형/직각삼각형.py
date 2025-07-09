2f727f812280e0f115abe7a84aa908dd5f008906
Yeong Hwan Jung <160090871+Jungyh0@users.noreply.github.com>
2025-07-09 10:49:08 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,15 @@
+import math
+import sys
+input = sys.stdin.readline
+
+while True:
+    m = list(map(int, input().split()))
+    
+    if sum(m) == 0:
+        break
+    m.sort()
+    if m[2] ** 2 == m[0] ** 2 + m[1] ** 2:
+        print("right")
+    else:
+        print("wrong") 
+    
