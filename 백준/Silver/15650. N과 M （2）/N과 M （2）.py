31ebe8e54b6d30f673efd6f21bedb6b4656ce5de
Yeong Hwan Jung <160090871+Jungyh0@users.noreply.github.com>
2025-11-24 15:54:38 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,15 @@
+n, m = map(int,input().split())
+
+result = []
+
+def dfs(start):
+
+    if len(result) == m:
+        print(*result)
+        return
+    for i in range(start, n + 1):
+        result.append(i)
+        dfs(i + 1)
+        result.pop()
+
+dfs(1)
